a60861304efd00041a115301c7c5e6c06a85b85f
Frederique Andolfatto <pk37fki@eduvaud.ch>
2026-02-01 18:20:52 +0100

    style used

---
+++ main.js
@@ -0,0 +1,8 @@
+const App = Vue.createApp({
+    data() {
+        return {
+            product : 'Voitures',
+            description : 'Modèle 849 Testarossa de chez Ferrari'
+        }
+    }
+})
